e7d138d7f3b5ee0da2671758470b580772e5feed
Olivier Van Acker <cyberroadie@gmail.com>
2017-07-31 01:21:11 +0100

    Bresenham for line drawing

--- heightmap/hmap.go
+++ heightmap/hmap.go
@@ -7,6 +7,7 @@ import (
 	"image/color"
 	"image/draw"
 	"log"
+	"math"
 )
 
 type HeightMap struct {
@@ -32,7 +33,7 @@ func (h *HeightMap) setInitialHeight(height float64) {
 	draw.Draw(h.img, h.img.Bounds(), &image.Uniform{getColor(height)}, image.ZP, draw.Src)
 }
 
-func (h *HeightMap) DrawLine(lineWidth int) {
+func (h *HeightMap) DrawSingleLine(lineWidth int) {
 	for i := 0; i < lineWidth; i++ {
 		h.drawSingleLine(h.img.Bounds().Max.X / 2 - lineWidth /2 + i)
 	}
@@ -44,6 +45,154 @@ func (h *HeightMap) drawSingleLine(offset int) {
 	}
 }
 
+func (h *HeightMap) DrawLine(x1, y1, x2, y2 int, height float64, radius int) {
+		h.Bresenham(x1, y1, x2, y2, radius, height)
+}
+
+func (h *HeightMap) Bresenham(x1, y1, x2, y2, radius int, height float64) {
+	var dx, dy, e, slope int
+
+	// Because drawing p1 -> p2 is equivalent to draw p2 -> p1,
+	// I sort points in x-axis order to handle only half of possible cases.
+	if x1 > x2 {
+		x1, y1, x2, y2 = x2, y2, x1, y1
+	}
+
+	dx, dy = x2-x1, y2-y1
+	// Because point is x-axis ordered, dx cannot be negative
+	if dy < 0 {
+		dy = -dy
+	}
+
+	switch {
+
+	// Is line a point ?
+	case x1 == x2 && y1 == y2:
+		h.drawCircle(x1, y1, radius, height)
+
+		// Is line an horizontal ?
+	case y1 == y2:
+		for ; dx != 0; dx-- {
+			h.drawCircle(x1, y1, radius, height)
+			x1++
+		}
+		h.drawCircle(x1, y1, radius, height)
+
+		// Is line a vertical ?
+	case x1 == x2:
+		if y1 > y2 {
+			y1, y2 = y2, y1
+		}
+		for ; dy != 0; dy-- {
+			h.drawCircle(x1, y1, radius, height)
+			y1++
+		}
+		h.drawCircle(x1, y1, radius, height)
+
+		// Is line a diagonal ?
+	case dx == dy:
+		if y1 < y2 {
+			for ; dx != 0; dx-- {
+				h.drawCircle(x1, y1, radius, height)
+				x1++
+				y1++
+			}
+		} else {
+			for ; dx != 0; dx-- {
+				h.drawCircle(x1, y1, radius, height)
+				x1++
+				y1--
+			}
+		}
+		h.drawCircle(x1, y1, radius, height)
+
+		// wider than high ?
+	case dx > dy:
+		if y1 < y2 {
+			// BresenhamDxXRYD(img, x1, y1, x2, y2, col)
+			dy, e, slope = 2*dy, dx, 2*dx
+			for ; dx != 0; dx-- {
+				h.drawCircle(x1, y1, radius, height)
+				x1++
+				e -= dy
+				if e < 0 {
+					y1++
+					e += slope
+				}
+			}
+		} else {
+			// BresenhamDxXRYU(img, x1, y1, x2, y2, col)
+			dy, e, slope = 2*dy, dx, 2*dx
+			for ; dx != 0; dx-- {
+				h.drawCircle(x1, y1, radius, height)
+				x1++
+				e -= dy
+				if e < 0 {
+					y1--
+					e += slope
+				}
+			}
+		}
+		h.drawCircle(x2, y2, radius, height)
+
+		// higher than wide.
+	default:
+		if y1 < y2 {
+			// BresenhamDyXRYD(img, x1, y1, x2, y2, col)
+			dx, e, slope = 2*dx, dy, 2*dy
+			for ; dy != 0; dy-- {
+				h.drawCircle(x1, y1, radius, height)
+				y1++
+				e -= dx
+				if e < 0 {
+					x1++
+					e += slope
+				}
+			}
+		} else {
+			// BresenhamDyXRYU(img, x1, y1, x2, y2, col)
+			dx, e, slope = 2*dx, dy, 2*dy
+			for ; dy != 0; dy-- {
+				h.drawCircle(x1, y1, radius, height)
+				y1--
+				e -= dx
+				if e < 0 {
+					x1++
+					e += slope
+				}
+			}
+		}
+		h.drawCircle(x2, y2, radius, height)
+	}
+}
+
+func (h *HeightMap) drawCircle(x1, y1, radius int, height float64) {
+	for y :=-radius; y<=radius; y++ {
+		for  x := -radius; x<=radius; x++ {
+			if (x*x+y*y <= radius*radius) {
+				h.img.SetGray16(x1 + x, y1 + y, getColor(height))
+			}
+		}
+	}
+}
+
+
+func (h *HeightMap) drawLine(x1, y1, x2, y2 int, height float64) {
+	var slope float64
+	if x2 - x1 == 0 {
+		slope = 0
+	} else {
+		slope = float64((y2 - y1)) / float64((x2 - x1))
+	}
+	b := float64(y1) - slope * float64(x1)
+
+	for x := x1; x < x2; x++ {
+		y := slope * float64(x) + b
+		h.img.SetGray16(x, int(y), getColor(height))
+	}
+
+}
+
 func getColor(c float64) color.Gray16 {
 	if c < 0 || c > 1.0 {
 		log.Fatalf("c value should be betweeen 0.0 and 1.0: %v", c)
@@ -51,3 +200,34 @@ func getColor(c float64) color.Gray16 {
 	intColor := uint16(float64(MAX_GREY) * c)
 	return color.Gray16{intColor}
 }
+
+func (h *HeightMap) DrawHex(x1, y1, size, radiusLine int, height float64) {
+	xs, ys := hexCorner(x1, y1, size, 1)
+
+	for i := 2; i < 8  ; i++ {
+		println("====")
+		println(i)
+		xe, ye := hexCorner(x1, y1, size, i)
+		println(xs)
+		println(ys)
+		println(xe)
+		println(ye)
+		h.DrawLine(xs, ys, xe, ye, height, radiusLine)
+		xs, ys = xe, ye
+
+	}
+}
+
+func hexCorner(x1, y1, size, i int) (x, y int){
+	angle_deg := float64(60 * i   + 30)
+	angle_rad := math.Pi / 180 * angle_deg
+	x = x1 + int(float64(size) * math.Cos(angle_rad))
+	y = y1 +int(float64(size)* math.Sin(angle_rad))
+	return
+}
+
+//function hex_corner(center, size, i):
+//var angle_deg = 60 * i   + 30
+//var angle_rad = PI / 180 * angle_deg
+//return Point(center.x + size * cos(angle_rad),
+//center.y + size * sin(angle_rad))
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import heightmap2 "terrain/heightmap"
 
 func main() {
 	heightmap := heightmap2.New(2048,2048)
-	heightmap.DrawLine(50)
+	//heightmap.DrawSingleLine(50)
+	//heightmap.DrawLine(1024, 1536, 581, 1279, 1.0, 1)
+	//heightmap.DrawLine(1024, 1024, 2048, 2048, 1.0, 10)
+	//heightmap.DrawLine(2048, 2048, 1024, 1024, 0.7, 10)
+	heightmap.DrawHex(1024, 1024, 512,10, 0.8)
 	heightmap.SaveImage("heightmap.png")
 }
